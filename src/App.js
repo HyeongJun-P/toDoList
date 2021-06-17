a849a1c7572af81098c8e1cdfc0ac9b5ecd30a43
Unknown-word <80656271+Unknown-word@users.noreply.github.com>
2021-06-17 11:50:46 +0900

    test 1.0
    최적화 진행중

--- src/App.js
+++ src/App.js
@@ -1,62 +1,121 @@
-import React, {useState, useRef, useCallback} from "react";
+import React, { useReducer, useRef, useCallback } from "react";
 import DoTemp from "./components/DoTemp";
 import DoInsert from "./components/DoInsert";
 import DoList from "./components/DoList";
 
-const App = () => {
+const createTodos = () => {
+  const array = [];
 
-  const [todos, setTodos] = useState([{
-    id:1,
-    text:'리액트의 기초 알아보기',
-    checked:true
-  },
-  {
-    id:2,
-    text:'컴포넌트 스타일링해 보기',
-    checked:true
-  },
-  {
-    id:3,
-    text:'일정 관리 앱 만들어 보기',
-    checked:false
-  }]);
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+};
 
-  const nextId = useRef(4);
-
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked:false,
-      }
-      setTodos(todos => todos.concat(todo));
-      nextId.current += 1;
-    }
-    ,[]
-  );
-
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id))
-    },
-    []
-  );
-
-  const onToggle = useCallback(
-    id => {
-      setTodos(todos =>
-        todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked}:todo)
+const DoReducer = (todos, action) => {
+  switch (action.type) {
+    case "INSERT":
+      return todos.concat(action.todo);
+    case "REMOVE":
+      return todos.filter((todo) => todo.id !== action.id);
+    case "TOGGLE":
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
       );
-    },[]
-  );
+    default:
+      return todos;
+  }
+};
+
+
+const App = () => {
+  const [todos, dispatch] = useReducer(DoReducer, undefined, createTodos);
+  
+  const nextId = useRef(2501);
+  
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: "INSERT", todo });
+    nextId.current += 1;
+  }, []);
+  
+  const onRemove = useCallback((id) => {
+    dispatch({ type: "REMOVE", id });
+  }, []);
+  
+  const onToggle = useCallback((id) => {
+    dispatch({ type: "TOGGLE", id });
+  }, []);
 
   return (
     <DoTemp>
-      <DoInsert onInsert={onInsert}/>
-      <DoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+      <DoInsert onInsert={onInsert} />
+      <DoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </DoTemp>
   );
-}
+};
 
-export default App
+export default App;
+
+// const App = () => {
+
+//   const [todos, setTodos] = useState([{
+//     id:1,
+//     text:'리액트의 기초 알아보기',
+//     checked:true
+//   },
+//   {
+//     id:2,
+//     text:'컴포넌트 스타일링해 보기',
+//     checked:true
+//   },
+//   {
+//     id:3,
+//     text:'일정 관리 앱 만들어 보기',
+//     checked:false
+//   }]);
+
+//   const nextId = useRef(4);
+
+//   const onInsert = useCallback(
+//     text => {
+//       const todo = {
+//         id: nextId.current,
+//         text,
+//         checked:false,
+//       }
+//       setTodos(todos => todos.concat(todo));
+//       nextId.current += 1;
+//     }
+//     ,[]
+//   );
+
+//   const onRemove = useCallback(
+//     id => {
+//       setTodos(todos => todos.filter(todo => todo.id !== id))
+//     },
+//     []
+//   );
+
+//   const onToggle = useCallback(
+//     id => {
+//       setTodos(todos =>
+//         todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked}:todo)
+//       );
+//     },[]
+//   );
+
+//   return (
+//     <DoTemp>
+//       <DoInsert onInsert={onInsert}/>
+//       <DoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+//     </DoTemp>
+//   );
+// }
--- src/components/DoList.js
+++ src/components/DoList.js
@@ -1,15 +1,43 @@
-import React, {memo} from "react";
+import React, {memo, useCallback} from "react";
 import DoLitem from "./DoLitem";
+import { List } from 'react-virtualized';
 import "./DoList.scss";
 
-const DoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="DoList">
-      {todos.map((todo) => (
-        <DoLitem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
-      ))}
-    </div>
+const DoList = ( {todos, onRemove, onToggle} ) => {
+
+  const rowRenderer = useCallback(
+    ({ index, key, style}) => {
+      const todo = todos[index];
+      return(
+        <DoLitem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos]
   );
-};
+  return (
+    <List 
+      className="DoList"
+      width={512}
+      list={todos}
+      rowCount={todos.length()}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      style= {{outline : 'none'}}
+    />
+  );
+}
 
 export default memo(DoList);
+
+
+// <div className="DoList">
+    //   {todos.map((todo) => (
+    //     <DoLitem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
+    //   ))}
+    // </div>
